892d370ee198dd363ab2469159444eb057c0e48b
JLBV <Jolobov.it@gmail.com>
2024-04-22 13:59:16 +0300

    feat: create Signup TDT-4

---
+++ src/main/java/com/example/api/controller/AuthenticationController.java
@@ -0,0 +1,24 @@
+package com.example.api.controller;
+
+import com.example.api.payload.request.SignupRequest;
+import com.example.api.service.AuthService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/api/auth")
+public class AuthenticationController {
+
+    private final AuthService authService;
+
+    public AuthenticationController(AuthService service) {
+        this.authService = service;
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> registerUser(@RequestBody SignupRequest request) {
+        return authService.registerUser(request.getUsername(), request.getPassword());
+    }
+
+}

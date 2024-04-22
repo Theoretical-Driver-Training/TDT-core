6086be1553ce9c662f38234b081d5b0e0957cd5a
JLBV <Jolobov.it@gmail.com>
2024-04-22 13:35:43 +0300

    feat: create User Entity TDT-10

---
+++ src/main/java/com/example/webapi/service/user/UserDetailsServiceImpl.java
@@ -0,0 +1,45 @@
+package com.example.webapi.service.user;
+
+import com.example.webapi.model.user.User;
+import com.example.webapi.repository.user.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private static final Logger logger = Logger.getLogger(UserDetailsServiceImpl.class.getName());
+
+    private final UserRepository repository;
+
+    public UserDetailsServiceImpl(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    public void saveUser(User user) {
+        repository.save(user);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = repository.findByUsername(username)
+                .orElseThrow(() -> {
+                    logger.log(Level.SEVERE, "Username {0} not found", username);
+                    return new UsernameNotFoundException("User not found with username: " + username);
+                });
+        return new UserDetailsImpl(user);
+    }
+
+    public boolean existsByUsername(String username) {
+        return repository.existsByUsername(username);
+    }
+
+    public void deleteUser(User user) {
+        repository.delete(user);
+    }
+}

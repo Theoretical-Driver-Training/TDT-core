f0a31d2ea4ef5a7df8f6bd4a33a8b199e582857c
JLBV <Jolobov.it@gmail.com>
2024-04-24 16:19:02 +0300

    fix: update mail message TDT-4

--- src/main/java/com/example/api/payload/response/PasswordChangeResponse.java
+++ src/main/java/com/example/api/payload/response/PasswordChangeResponse.java
@@ -21,7 +21,7 @@ public class PasswordChangeResponse {
         content = "<html><body>"
                 + "<p style='font-family: Arial, sans-serif;'>Приветствуем вас в системе оценки функциональной готовности водителей автотранспорта!</p>"
                 + "<p style='font-family: Arial, sans-serif;'>Вы получили это письмо, потому что инициировали процесс изменения пароля для вашей учетной записи. Если вы не делали такого запроса, немедленно свяжитесь с администрацией системы.</p>"
-                + "<p style='font-family: Arial, sans-serif;'><strong>Для установки нового пароля, пожалуйста, активируйте вашу учетную запись.</strong> После активации вы будете перенаправлены на страницу, где сможете установить и подтвердить ваш новый пароль.</p>"
+                + "<p style='font-family: Arial, sans-serif;'><strong>Для установки нового пароля, перейдите по ссылке ниже.</strong> После перехода вы сможете установить и подтвердить ваш новый пароль.</p>"
                 + "<div style='text-align: center; margin-top: 20px;'>"
                 + "    <a href='" + confirmationLink + "' style='background-color: #4CAF50; color: white; padding: 10px 20px; text-align: center; text-decoration: none; display: inline-block; border-radius: 5px; font-family: Arial, sans-serif;'>Установить новый пароль</a>"
                 + "</div>"
--- src/main/java/com/example/api/payload/response/PasswordForgotResponse.java
+++ src/main/java/com/example/api/payload/response/PasswordForgotResponse.java
@@ -19,9 +19,9 @@ public class PasswordForgotResponse {
         content = "<html><body>"
                 + "<p style='font-family: Arial, sans-serif;'>Приветствуем вас в системе оценки функциональной готовности водителей автотранспорта!</p>"
                 + "<p style='font-family: Arial, sans-serif;'>Вы получили это письмо, потому что запросили восстановление доступа к вашей учетной записи. Если вы не делали такого запроса, просто проигнорируйте это сообщение. В случае повторения таких писем, рекомендуем обратиться к администратору системы.</p>"
-                + "<p style='font-family: Arial, sans-serif;'><strong>Активируйте вашу учетную запись.</strong> После активации вы будете перенаправлены на страницу, где сможете установить новый пароль.</p>"
+                + "<p style='font-family: Arial, sans-serif;'><strong>Для восстановления доступа к учетной записи, перейдите по ссылке ниже.</strong> После перехода вы сможете установить новый пароль.</p>"
                 + "<div style='text-align: center; margin-top: 20px;'>"
-                + "    <a href='" + activationLink + "' style='background-color: #4CAF50; color: white; padding: 10px 20px; text-align: center; text-decoration: none; display: inline-block; border-radius: 5px; font-family: Arial, sans-serif;'>Активировать учетную запись</a>"
+                + "    <a href='" + activationLink + "' style='background-color: #4CAF50; color: white; padding: 10px 20px; text-align: center; text-decoration: none; display: inline-block; border-radius: 5px; font-family: Arial, sans-serif;'>Восстановить пароль</a>"
                 + "</div>"
                 + "<p style='font-family: Arial, sans-serif;'>Обратите внимание: это письмо сформировано автоматически и ваш ответ на него не будет прочитан.</p>"
                 + "</body></html>";

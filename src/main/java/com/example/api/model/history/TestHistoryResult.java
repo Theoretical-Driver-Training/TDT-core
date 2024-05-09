1dd17e46e2bf8bcc2281f4e8911ccbc614450bac
JLBV <Jolobov.it@gmail.com>
2024-05-09 14:14:26 +0300

    feat: create Test History TDT-27

---
+++ src/main/java/com/example/api/model/history/TestHistoryResult.java
@@ -0,0 +1,32 @@
+package com.example.api.model.history;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "test_history_results")
+public class TestHistoryResult {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "test_history_id", nullable = false)
+    private TestHistory testHistory;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private int value;
+
+    @Column(nullable = false)
+    private String description;
+
+}

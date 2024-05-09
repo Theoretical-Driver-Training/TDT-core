c9787a074afbe7f610daf1ef51099395e3d23381
JLBV <Jolobov.it@gmail.com>
2024-05-09 18:38:04 +0300

    fix: create Async config for Mail sender TDT-1

--- src/main/java/com/example/api/service/mail/MailService.java
+++ src/main/java/com/example/api/service/mail/MailService.java
@@ -2,22 +2,22 @@ package com.example.api.service.mail;
 
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 
 @Service
+@Slf4j
 public class MailService {
-
-    private static final Logger logger = LoggerFactory.getLogger(MailService.class);
 
     @Autowired
     private JavaMailSender mailSender;
 
+    @Async("asyncExecutor")
     public void sendMail(String to, String subject, String text) {
         MimeMessage mimeMessage = mailSender.createMimeMessage();
         try {
@@ -27,9 +27,9 @@ public class MailService {
             helper.setText(text, true);
 
             mailSender.send(mimeMessage);
-            logger.info("Email sent successfully to {}", to);
+            log.info("Email sent successfully to {}", to);
         } catch (MessagingException e) {
-            logger.error("Failed to send email to {}", to, e);
+            log.error("Failed to send email to {}", to, e);
             throw new RuntimeException("Failed to send email to: " + to, e);
         }
     }

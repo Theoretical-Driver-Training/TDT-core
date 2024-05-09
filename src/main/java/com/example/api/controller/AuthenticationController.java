86465ad61a61c3237561e095e3b4ba51391cd54d
JLBV <Jolobov.it@gmail.com>
2024-05-09 16:49:53 +0300

    fix: fix Auth TDT-1

--- src/main/java/com/example/api/controller/AuthenticationController.java
+++ src/main/java/com/example/api/controller/AuthenticationController.java
@@ -1,8 +1,8 @@
 package com.example.api.controller;
 
-import com.example.api.payload.request.SigninRequest;
-import com.example.api.payload.request.SignupRequest;
+import com.example.api.payload.request.AuthRequest;
 import com.example.api.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -12,20 +12,21 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/auth")
 public class AuthenticationController {
 
-    private final AuthService authService;
-
-    public AuthenticationController(AuthService service) {
-        this.authService = service;
-    }
+    @Autowired
+    private AuthService authService;
 
     @PostMapping("/signup")
-    public ResponseEntity<?> registerUser(@RequestBody SignupRequest request) {
-        return authService.registerUser(request.getUsername(), request.getPassword());
+    public ResponseEntity<?> registerUser(@RequestBody AuthRequest request) {
+        String username = request.getUsername();
+        String password = request.getPassword();
+        return authService.registerUser(username, password);
     }
 
     @PostMapping("/signin")
-    public ResponseEntity<?> authenticateUser(@RequestBody SigninRequest request) {
-        return authService.authenticateUser(request.getUsername(), request.getPassword());
+    public ResponseEntity<?> authenticateUser(@RequestBody AuthRequest request) {
+        String username = request.getUsername();
+        String password = request.getPassword();
+        return authService.authenticateUser(username, password);
     }
 
     @PostMapping("/logout")
